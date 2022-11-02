dd617d0e78a2e759c3eef425d796ca84683c9bf1
Michelle Barker <mbarker_84@hotmail.co.uk>
2022-11-02 08:46:45 +0000

    Async component

---
+++ vue/components/AsyncTest.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ data }}</p>
+    <div class="widget" :style="{ 'aspect-ratio': loading ? aspectRatio : '' }">
+      <slot :data="data" v-if="data"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    data: Array,
+  },
+
+  setup(props) {
+    const data = props.data;
+
+    return { data };
+  },
+});
+</script>
--- vue/routes/Home.vue
+++ vue/routes/Home.vue
@@ -2,6 +2,9 @@
   <div class="container">
     <h1>Home</h1>
     <div class="grid">
+      <component :is="TestComponent" :data="data">
+        <KPI :data="data" color="turquoise"></KPI>
+      </component>
       <WidgetLoader
         url="https://api.npoint.io/01264aa2a713715ccc1e"
         v-slot="slotProps"
@@ -31,18 +34,50 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, defineAsyncComponent, ref } from "vue";
 import DataTable from "../components/DataTable";
 import KPI from "../components/KPI";
 import WidgetLoader from "../components/WidgetLoader";
+import Loader from "../components/Loader";
+import Error from "../components/Error";
 
 export default defineComponent({
   components: {
     DataTable,
     KPI,
     WidgetLoader,
+    // AsyncTest: () => import("../components/AsyncTest.vue"),
+    Loader,
+    Error,
   },
 
-  setup() {},
+  setup() {
+    const data = ref(null);
+
+    const fetchAndLoad = async () => {
+      try {
+        const response = await fetch(
+          "https://api.npoint.io/01264aa2a713715ccc1e"
+        );
+        data.value = await response.json();
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    const loaderFn = () => {
+      return fetchAndLoad().then(() => import("../components/AsyncTest.vue"));
+    };
+
+    const TestComponent = defineAsyncComponent({
+      loader: loaderFn,
+      loadingComponent: Loader,
+      errorComponent: Error,
+      delay: 1000,
+      timeout: 4000,
+    });
+
+    return { TestComponent, data };
+  },
 });
 </script>
