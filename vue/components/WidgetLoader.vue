5801ea04b3110223d9b490a0f191482c69def92a
Michelle Barker <mbarker_84@hotmail.co.uk>
2022-11-03 10:59:34 +0000

    Add swrv library, WIP

--- vue/components/WidgetLoader.vue
+++ vue/components/WidgetLoader.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="widget" :style="{ 'aspect-ratio': loading ? aspectRatio : '' }">
-    <Loader v-if="loading"></Loader>
+  <div class="widget" :style="{ '--ar': isValidating ? aspectRatio : '' }">
+    <Loader v-if="isValidating"></Loader>
     <Error v-else-if="error" :errorMessage="error"></Error>
-    <slot :data="data" v-if="data"></slot>
+    <slot :data="data" v-else-if="data"></slot>
   </div>
 </template>
 
@@ -10,6 +10,8 @@
 import { defineComponent, ref, onBeforeMount, onBeforeUnmount } from "vue";
 import Loader from "./Loader";
 import Error from "./Error";
+import useSWRV from "swrv";
+import LocalStorageCache from "swrv/dist/cache/adapters/localStorage";
 
 export default defineComponent({
   components: { Loader, Error },
@@ -25,30 +27,46 @@ export default defineComponent({
   setup(props) {
     const { url, aspectRatio } = props;
     const loading = ref(true);
-    const error = ref(null);
-    const data = ref(null);
-    const controller = ref(null);
+    // const error = ref(null);
+    // const data = ref(null);
+    // const controller = ref(null);
+    const controller = new AbortController();
 
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url, { signal: controller.value.signal });
-        const jsonData = await response.json();
-        data.value = jsonData;
-      } catch (err) {
-        error.value = err.message;
-      } finally {
-        loading.value = false;
-      }
+    const fetchFn = () => {
+      return fetch(url, { signal: controller.signal })
+        .then((response) => response.json())
+        .catch((e) => console.error(e))
+        .finally(() => (loading.value = false));
     };
 
+    // const fetchData = async () => {
+    //   try {
+    //     const response = await fetch(url, { signal: controller.value.signal });
+    //     const jsonData = await response.json();
+    //     data.value = jsonData;
+    //   } catch (err) {
+    //     error.value = err.message;
+    //   } finally {
+    //     loading.value = false;
+    //   }
+    // };
+
+    const { data, error, isValidating } = useSWRV(url, fetchFn);
+
     onBeforeMount(() => {
-      controller.value = new AbortController();
-      fetchData();
+      // controller.value = new AbortController();
+      const swrvFetch = useSWRV(url, fetchFn, {
+        cache: new LocalStorageCache("swrv"),
+        shouldRetryOnError: false,
+      });
+      data.value = swrvFetch.data;
+      error.value = swrvFetch.error;
+      // fetchData();
     });
 
-    onBeforeUnmount(() => controller.value.abort());
+    onBeforeUnmount(() => controller.abort());
 
-    return { loading, error, data, aspectRatio };
+    return { loading, error, data, aspectRatio, isValidating };
   },
 });
 </script>
