8b38dd57026b0b146f5c3482ca5d383e0ae44f62
Michelle Barker <mbarker_84@hotmail.co.uk>
2022-11-08 16:04:49 +0000

    Add Github link

--- vue/App.vue
+++ vue/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <nav>
-    <ul class="nav__list">
-      <li v-for="link in links" :key="link.to">
-        <router-link :to="link.to" class="nav__link">{{
-          link.title
-        }}</router-link>
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <nav>
+      <ul class="nav__list">
+        <li v-for="link in links" :key="link.to">
+          <router-link :to="link.to" class="nav__link">{{
+            link.title
+          }}</router-link>
+        </li>
+      </ul>
+    </nav>
+    <a
+      class="header__github-link"
+      href="https://github.com/mbarker84/vue-app-example"
+      >View on Github</a
+    >
+  </header>
   <main>
     <router-view></router-view>
   </main>
